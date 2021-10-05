a594fc0e0c15c02a0fcadb0e4cbda6aef89b54d8
Machfar <souhayel.isetch@gmail.com>
2021-10-05 17:55:47 +0100

    About page done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 import Header from "../components/Layout/Header";
+import Story from "../components/Shared/About/Story";
+import LeadingBrand from "../components/Shared/About/LeadingBrand";
+import Team from "../components/Shared/About/Team";
 import Footer from "../components/Layout/Footer";
 import {} from "./../components/Shared";
 import bgGames from "./../assets/images/bgAbout.svg";
@@ -9,6 +12,9 @@ function About() {
   return (
     <div className={styles["About-page"]}>
       <Header title="About us" pageLocation="Home > About Us" bgImg={bgGames} />
+      <Story />
+      <LeadingBrand />
+      <Team />
       <Footer />
     </div>
   );
